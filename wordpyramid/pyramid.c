e74048d432c59afd549a593bcf3d698134368091
Lickton <lilihuanmao@gmail.com>
2021-01-17 11:25:57 +0800

    添加了一些注释

--- wordpyramid/pyramid.c
+++ wordpyramid/pyramid.c
@@ -2,10 +2,10 @@
 #include <math.h>
 int main(void)
 {
-    int i, n, q, j;
-    int delta;
-    char aword;
-    char ch[26] = "0";
+    int i, n, q, j;                                   //计数器大集合
+    int delta;                                        //所输入字母 与 ‘A’ + 中间所有数总共几个
+    char aword;                                       //要求输入的字母，即打印最后一行的中间字母
+    char ch[26] = "0";                                //初始化包含26个字母的数组
     for(q=0; q<26; q++)
         ch[q] = 'A' + q;
     printf("Please input a word:");
@@ -17,7 +17,7 @@ int main(void)
     {
         for(i=1; i<=delta-n; i++)
             printf(" ");
-        for(j=n-1; (n-1)-(int)fabs(j)>=0; j--)
+        for(j=n-1; (n-1)-(int)fabs(j)>=0; j--)          //应当注意数组越界问题，否则会“烫烫烫烫烫烫烫烫烫烫烫烫”！！！
             printf("%c", ch[(n-1)-(int)fabs(j)]);
         for(i=1; i<=delta-n; i++)
             printf(" ");
