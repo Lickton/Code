cc4d11143f35292de4807c6a9b6d3eac996ba8ab
Lickton <lilihuanmao@gmail.com>
2021-02-04 11:10:55 +0800

    P1001

---
+++ Luogu_P1001.cpp
@@ -0,0 +1,16 @@
+/*
+ * 洛谷
+ * 2021.2.4
+ * github @Lickton
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d",&a,&b);
+    printf("%d", a+b);
+    
+    return 0;
+}
