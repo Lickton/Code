05c1e4b84afee5bf2f9a5ff73d02d9e42cbaf693
Lickton <lilihuanmao@gmail.com>
2021-02-26 17:17:37 +0800

    init

---
+++ PAT1001.c
@@ -0,0 +1,23 @@
+/*
+ * PAT
+ * 2021.2.26
+ * github @Lickton
+ **/
+#include <stdio.h>
+int main(void)
+{
+    int n, count = 0;
+    scanf("%d", &n);
+
+    while(n!=1)
+    {
+        if( n%2==0 )
+            n = n / 2;
+        else
+            n = (3*n+1) / 2;
+        count++;
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
