afd9cc0286d9a6aef69114d40fffcb7afc3010ce
Lickton <lilihuanmao@gmail.com>
2021-03-27 16:17:03 +0800

    init

---
+++ 团体程序设计天梯赛/计算摄氏温度.cpp
@@ -0,0 +1,17 @@
+/*
+ * 团体程序设计天梯赛
+ * 2021.3.26
+ * github @Lickton
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int F, C;
+    cin >> F;
+    C = 5 * ( F - 32) / 9;
+    cout << "Celsius = " << C;
+
+    return 0;
+}
