63e51f441a2b8a74797d2fcb2aec5d609f3f617c
Lickton <lilihuanmao@gmail.com>
2021-03-29 17:36:36 +0800

    init

--- 团体程序设计天梯赛/考试座位号.cpp
+++ 团体程序设计天梯赛/考试座位号.cpp
@@ -1,12 +1,38 @@
 /*
  * 团体程序设计天梯赛
- * 2021.3.26
+ * 2021.3.29
  * github @Lickton
  * */
 #include <bits/stdc++.h>
 using namespace std;
+struct info
+{
+    string Num;
+    int Text, Exam;
+};
 
 int main()
 {
-    
+    int n, m;
+    cin >> n;
+    vector<info> a(n);
+    for(int i=0; i<n; i++)
+        cin >> a[i].Num >> a[i].Text >> a[i].Exam;
+    cin >> m;
+    vector<int> Inquiry(m);
+    for(int i=0; i<m; i++)
+        cin >> Inquiry[i];
+    for(int j=0; j<m; j++)
+    {
+        for( int i=0; i<n; i++)
+        {
+            if(a[i].Text == Inquiry[j])
+            {
+                cout << a[i].Num << " " << a[i].Exam << endl;
+                break;
+            }
+        }
+    }
+
+    return 0;
 }
