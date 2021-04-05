47c5d48667f1cc0b602e88fadf2759c048734556
Lickton <lilihuanmao@gmail.com>
2021-04-05 21:45:02 +0800

    爷青结，安大有OJ了

---
+++ AHUOJ/人见人爱的A+B.cpp
@@ -0,0 +1,19 @@
+/*
+ * 洛谷
+ * 2021.4.5
+ * github @Lickton
+ * */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int sum;
+    cin >> a >> b;
+    
+    sum = a + b;
+    cout << sum << endl;
+
+    return 0; 
+}
